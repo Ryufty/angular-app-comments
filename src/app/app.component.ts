138797383e791b60fe8a5d6e76be0aeb5f18a65c
chiuleung <chiu-leung@Hotmail.com>
2018-07-30 15:39:45 -0400

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,80 @@
 import { Component } from '@angular/core';
+import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument } from 'angularfire2/firestore';
+import { Observable } from 'rxjs';
+import { map, take, debounceTime } from 'rxjs/operators';
+import { ReactiveFormsModule, FormGroup, FormBuilder, Validators } from '@angular/forms';
+
+interface Post {
+  title: string;
+  content: string;
+}
+
+interface PostId extends Post { 
+  id: string; 
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'app';
+  postsCol: AngularFirestoreCollection<Post>;
+  //posts: Observable<Post[]>;
+  posts: any;
+  todayDate: any;
+ 
+  postDoc: AngularFirestoreDocument<Post>;
+  post: Observable<Post>;
+
+  commentForm: FormGroup;
+  
+  constructor (
+    private afs: AngularFirestore,
+    private fb: FormBuilder) {}
+
+  ngOnInit() {
+    // For setting up "CRITERIA"
+    // this.postsCol = this.afs.collection('posts', ref => ref.where('title', '==', 'coursetro'));
+    this.postsCol = this.afs.collection('posts');
+    this.posts = this.postsCol.snapshotChanges().pipe(
+      map(actions => {
+        return actions.map(a => {
+          const data = a.payload.doc.data() as Post;
+          const id = a.payload.doc.id;
+
+          return { id, data };
+        });
+      })
+    );
+
+    this.todayDate = Date.now();
+
+    this.commentForm = this.fb.group ({
+      title: ['', [Validators.required]],
+      content: ['', [Validators.required]],
+    });
+  }
+
+  get title() { return this.commentForm.get('title'); }
+  get content() { return this.commentForm.get('content'); }
+
+  addPost() {
+    this.afs.collection('posts').add({ 'title': this.title.value, 'content': this.content.value });
+    // this.afs.collection('posts')
+    //   .doc('my-custom-id')
+    //   .set( {'title': this.title.value, 'content': this.content.value} );
+    this.commentForm.reset();
+  }
+
+  getPost(postId) {
+    this.postDoc = this.afs.doc('posts/'+postId);
+    this.post = this.postDoc.valueChanges();
+  }
+
+  deletePost(postId) {
+    this.afs.doc('posts/'+postId).delete();
+  }
+
 }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDMafiL7mlv_LGQ6GWRkWBZh_rIEXvYHc4",
+    authDomain: "firestore-30878.firebaseapp.com",
+    databaseURL: "https://firestore-30878.firebaseio.com",
+    projectId: "firestore-30878",
+    storageBucket: "firestore-30878.appspot.com",
+    messagingSenderId: "325291853674"
+  }
 };
